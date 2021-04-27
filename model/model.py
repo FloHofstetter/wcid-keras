36e559186a292fbb70749349273d4427140dcfac
Florian Hofstetter <flo.max.hofstetter@gmail.com>
2021-04-27 16:55:10 +0200

    Add:   - Input resolution as dynamic parameter   - Parameter in docstring
    Changed:
      - Output layer activation from "relu" to "sigmoid"
      - Batch normalisation is now used
      - Removed unused Class import

--- model/model.py
+++ model/model.py
@@ -1,21 +1,28 @@
 from tensorflow import keras
-from tensorflow.keras.layers import Activation, Dropout, UpSampling2D
+from tensorflow.keras.layers import Dropout, UpSampling2D
 from tensorflow.keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
+import numpy as np
 
 
-def get_model():
+def get_model(input_shape=(320, 160)):
     """
     Get the CNN network model
 
+    :param input_shape: Shape of the input image.
     :return: Network model.
     """
+    # Change width and height for keras and append channels
+    input_shape = list(input_shape)
+    input_shape.append(3)
+    input_shape[0], input_shape[1] = input_shape[1], input_shape[0]
+
     # Create model
     model = keras.models.Sequential()
 
     # Down 1
-    # model.add(BatchNormalization(input_shape=(160, 320, 3)))
-    model.add(Conv2D(8, (3, 3), padding='valid', strides=(1, 1), activation='relu', input_shape=(160, 320, 3)))
+    model.add(BatchNormalization(input_shape=input_shape))
+    model.add(Conv2D(8, (3, 3), padding='valid', strides=(1, 1), activation='relu'))
     model.add(Conv2D(16, (3, 3), padding='valid', strides=(1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -68,6 +75,6 @@ def get_model():
     # Up 4
     model.add(UpSampling2D(size=(2, 2)))
     model.add(Conv2DTranspose(16, (3, 3), padding='valid', strides=(1, 1), activation='relu'))
-    model.add(Conv2DTranspose(1, (3, 3), padding='valid', strides=(1, 1), activation='relu'))
+    model.add(Conv2DTranspose(1, (3, 3), padding='valid', strides=(1, 1), activation='sigmoid'))
 
     return model
