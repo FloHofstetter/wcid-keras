a58f26b081084e7e792143b4e176fdd0da32bfc3
Florian Hofstetter <flo.max.hofstetter@gmail.com>
2021-05-09 15:25:07 +0200

    Change:   - Type annotations   - Docstring main function

--- dataset/augment.py
+++ dataset/augment.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List, Tuple
 import pathlib
 import random
 
@@ -13,7 +13,7 @@ def augment_images(
     bg_dir_pth: Union[pathlib.Path, str],
     bg_ext: str,
     p: float = 1.0,
-):
+) -> np.ndarray:
     """
     Blend in random background image.
     Separation of foreground and background is defined
@@ -29,23 +29,24 @@ def augment_images(
     :return: Augmented image as numpy array.
     """
     # Random number to decide if augmentation is applied.
-    rand_nr = random.random()
+    rand_nr: float = random.random()
 
     # Apply augmentation.
     if rand_nr < p:
         # Get path if string provided.
         bg_dir_pth = pathlib.Path(bg_dir_pth)
         # Convert RGB input to cv2 native color order
-        fg_arr = cv2.cvtColor(fg_arr, cv2.COLOR_RGB2BGR)
+        fg_arr: np.ndarray = cv2.cvtColor(fg_arr, cv2.COLOR_RGB2BGR)
 
         # Collect available background images.
-        bg_pths = list(bg_dir_pth.glob(f"*.{bg_ext}"))
+        bg_pths: List = list(bg_dir_pth.glob(f"*.{bg_ext}"))
 
         # Choose random background and open
         # and resize to foreground.
+        bg_pth: Union[pathlib.Path, str]
         bg_pth = random.choice(bg_pths)
-        bg_arr = cv2.imread(str(bg_pth))
-        fg_size = tuple(reversed(fg_arr.shape[:2]))
+        bg_arr: np.ndarray = cv2.imread(str(bg_pth))
+        fg_size: Tuple = tuple(reversed(fg_arr.shape[:2]))
         bg_arr = cv2.resize(bg_arr, fg_size)
 
         # Invert mask and spread contrast
@@ -53,7 +54,9 @@ def augment_images(
 
         # Calculate distance to label and min max scale
         # distance.
+        dist_arr: np.ndarray
         dist_arr, _ = cv2.distanceTransformWithLabels(msk_arr, cv2.DIST_L2, 5)
+        # TODO: Clean solution for zero division case.
         if (dist_arr.max() - dist_arr.min()) > 0:
             dist_arr = (dist_arr - dist_arr.min()) / (dist_arr.max() - dist_arr.min())
         else:
@@ -66,7 +69,7 @@ def augment_images(
 
         # Blend foreground and background.
         dist_arr = 1 - (1 - dist_arr) ** 3
-        blended = (1 - dist_arr) * fg_arr + dist_arr * bg_arr
+        blended: np.ndarray = (1 - dist_arr) * fg_arr + dist_arr * bg_arr
 
         # Back to uint8 range.
         blended = (blended * 255).astype(np.uint8)
@@ -81,24 +84,30 @@ def augment_images(
     return blended
 
 
-def main():
+def main() -> None:
+    """
+    Program entry point.
+
+    :return: None
+    :rtype: None
+    """
     # Path to images.
-    fg_pth = pathlib.Path("")
-    bg_pth = pathlib.Path("")
-    bg_ext = "jpg"
-    msk_pth = pathlib.Path("")
+    fg_pth: pathlib.Path = pathlib.Path("")
+    bg_pth: pathlib.Path = pathlib.Path("")
+    bg_ext: str = "jpg"
+    msk_pth: pathlib.Path = pathlib.Path("")
 
     # Open images.
-    fg_img = Image.open(fg_pth)
-    msk_img = Image.open(msk_pth)
-    fg_arr = np.asarray(fg_img)
-    msk_arr = np.asarray(msk_img)
+    fg_img: Image.Image = Image.open(fg_pth)
+    msk_img: Image.Image = Image.open(msk_pth)
+    fg_arr: np.ndarray = np.asarray(fg_img)
+    msk_arr: np.ndarray = np.asarray(msk_img)
 
     # Augment images.
-    blended = augment_images(fg_arr, msk_arr, bg_pth, bg_ext)
+    blended: np.ndarray = augment_images(fg_arr, msk_arr, bg_pth, bg_ext)
 
     # Show blended image.
-    blended_img = Image.fromarray(blended)
+    blended_img: Image.Image = Image.fromarray(blended)
     blended_img.show()
 
 
