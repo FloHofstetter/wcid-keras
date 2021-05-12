9f88d7c8b79d2af8d06f423013ca77b49b082a48
Florian Hofstetter <flo.max.hofstetter@gmail.com>
2021-05-12 19:16:10 +0200

    Change:   - Layer list notation.   - Network architecture stays same.

--- model/model.py
+++ model/model.py
@@ -18,83 +18,72 @@ def get_model(input_shape=(320, 160)):
     input_shape[0], input_shape[1] = input_shape[1], input_shape[0]
 
     # Create model
-    model = keras.models.Sequential()
-
-    # Down 1
-    model.add(BatchNormalization(input_shape=input_shape))
-    model.add(Conv2D(8, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Conv2D(16, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    # Down 2
-    model.add(Conv2D(16, (5, 5), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(32, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(32, (5, 5), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    # Down 3
-    model.add(Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(64, (5, 5), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    # Down 4
-    model.add(Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(64, (5, 5), padding="valid", strides=(1, 1), activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    # Up 1
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(
-        Conv2DTranspose(64, (5, 5), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-    model.add(
-        Conv2DTranspose(64, (3, 3), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-
-    # Up 2
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(
-        Conv2DTranspose(64, (5, 5), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-    model.add(
-        Conv2DTranspose(64, (3, 3), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-
-    # Up 3
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(
-        Conv2DTranspose(32, (5, 5), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-    model.add(
-        Conv2DTranspose(32, (3, 3), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-    model.add(
-        Conv2DTranspose(16, (5, 5), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(Dropout(0.2))
-
-    # Up 4
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(
-        Conv2DTranspose(16, (3, 3), padding="valid", strides=(1, 1), activation="relu")
-    )
-    model.add(
+    layers = [
+        # Down1
+        BatchNormalization(input_shape=input_shape),
+        Conv2D(8, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Conv2D(16, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        MaxPooling2D(pool_size=(2, 2)),
+        # Down2
+        Conv2D(16, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2D(32, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2D(32, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        MaxPooling2D(pool_size=(2, 2)),
+        # Down3
+        Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2D(64, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        MaxPooling2D(pool_size=(2, 2)),
+        # Down4
+        Conv2D(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2D(64, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        MaxPooling2D(pool_size=(2, 2)),
+        # Up1
+        UpSampling2D(size=(2, 2)),
+        Conv2DTranspose(64, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2DTranspose(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        # Up2
+        UpSampling2D(size=(2, 2)),
+        Conv2DTranspose(64, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2DTranspose(64, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        # Up3
+        UpSampling2D(size=(2, 2)),
+        Conv2DTranspose(32, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2DTranspose(32, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        Conv2DTranspose(16, (5, 5), padding="valid", strides=(1, 1), activation="relu"),
+        Dropout(0.2),
+        # Up4
+        UpSampling2D(size=(2, 2)),
+        Conv2DTranspose(16, (3, 3), padding="valid", strides=(1, 1), activation="relu"),
         Conv2DTranspose(
             1, (3, 3), padding="valid", strides=(1, 1), activation="sigmoid"
-        )
-    )
+        ),
+    ]
+
+    model = conv_model = keras.models.Sequential(layers)
 
     return model
+
+
+def main():
+    """
+
+    :return:
+    """
+    model = get_model()
+    model.summary()
+
+
+if __name__ == "__main__":
+    main()
